a14902dc17a1b0d6f962d8be549dd44fb187aa1a
Revel Yusuf <crazyprogrammer109@gmail.com>
2017-04-16 23:50:39 +0600

    revelyusuf.net
    This is the home page of my own website that i have designed.I used
    Jinja templates  for this.This is my first Django repositories.

---
+++ revelsite/personal/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'personal/home.html')
+
+def contact(request):
+    return render(request,'personal/basic.html', {'content':['If u want to met me thant email me','revelyusuf109@gmail.com']})
+
